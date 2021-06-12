214090d244161ba1573b2a991ab57367d47203f6
nataliekalantaryan <85220256+nataliekalantaryan@users.noreply.github.com>
2021-06-12 19:35:50 +0400

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,39 @@
+3.
+let number1 = prompt ("First number.")
+let number2 = prompt ("Second number")
+let number3 = prompt ("Third number")
+let number4 = prompt ("Fourth number")
+let number5 = prompt ("Fifth number")
+    let a = (number1 + number2 + number3 + number4 + number5)
+console.log (a / 5)
+// number1- number5y cragirn yndunum e vorpes string, patchary chem haskanum
+
+4.
+const number1 = prompt ("First number.")
+const number2 = prompt ("Second number.")
+const number3 = prompt ("Third number.")
+    if (number1 < number2 <number3) {
+        console.log (number1, number2, number3)
+    } else if (number1 < number3 < number2) {
+        console.log (number1, number3, number2)
+    } else if (number2 < number1 < number3) {
+        console.log (number2, number1, number3)
+    } else if (number2 < number3 < number1){
+        console.log (number2, number3, number1)
+    } else if (number3 < number1 < number2) {
+        console.log (number3, number1, number2)
+    } else if (number3 < number2 < number1) {
+        console.log (number3, number2, number1)
+    }
+    // aystex nuynpes tvery vorpes string en yndunvum
+5. 
+var n= +prompt()
+var i = 0
+var j = 0
+	if(n % 2 === 0 && !Math.floor(n / 10)){
+i += 1
+	}
+	if(n % 3 === 0 && n % 10 === 1){
+    j += 1
+    	} 
+
